45399f44fd822510c25e9c1fb5952cb4ebc79097
kadelcode <kadelcode@gmail.com>
2025-02-19 19:40:00 +0100

    Configuration files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: 'class', // Enabling dark mode
-  content: [],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
   plugins: [],
-}
+};
