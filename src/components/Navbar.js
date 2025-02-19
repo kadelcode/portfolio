ef71de5df2eb4cc60e7df107bd903103e8e2ee83
kadelcode <kadelcode@gmail.com>
2025-02-20 00:29:31 +0100

    Add new styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import ThemeToggle from './ThemeToggle';
 
 const Navbar = () => {
   return (
-    <nav className="p-4 flex justify-between items-center text-black bg-purple-500">
+    <nav className="p-4 flex justify-between items-center text-white z-10 bg-gray-600 backdrop-blur-sm shadow-lg">
       <Link to="/" className="font-bold text-xl">KADEL</Link>
       <div className="flex items-center space-x-4">
         <Link to="/projects" className="hover:text-blue-500">Projects</Link>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,9 +7,16 @@ const Home = () => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      className="min-h-screen flex items-center justify-center p-4"
+      className="min-h-screen h-full flex items-center justify-center p-4 relative"
     >
-      <div className="text-center">
+      <div
+      className='absolute inset-0 z-0 mb-0'
+      style={{
+        backgroundImage: `url('/images/kadel.jpg')`,
+        backgroundSize: 'cover',
+      }}
+      ></div>
+      <div className="text-center z-10 text-white">
         <h1 className="text-4xl font-bold mb-4">Welcome to My Portfolio</h1>
         <p className="text-lg">I'm a passionate developer...</p>
       </div>
