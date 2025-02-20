31b9c61ba4fc62ad4748aa3a79030c9269cdf092
kadelcode <kadelcode@gmail.com>
2025-02-20 17:01:58 +0100

    TypingEffect component

---
+++ src/components/TypingEffect.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useRef } from "react";
+import Typed from "typed.js";
+
+const TypingEffect = () => {
+  const el = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ["Hi, I'm Ayodele.", "It's great to have you here on my portfolio site!"],
+      typeSpeed: 50,
+      backSpeed: 25,
+      loop: false, // No looping
+      showCursor: true, // Initially show cursor
+      onComplete: () => {
+        document.querySelector(".typed-cursor")?.remove(); // Remove cursor after typing
+      }
+    });
+
+    return () => typed.destroy();
+  }, []);
+
+  return <span ref={el} />;
+};
+
+export default TypingEffect;
