eabe71cc1f2289388a83f3931f344cb2bd504d8a
kadelcode <kadelcode@gmail.com>
2025-02-20 14:58:32 +0100

    Update navbar color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import ThemeToggle from './ThemeToggle';
 
 const Navbar = () => {
   return (
-    <nav className="backdrop-blur-md bg-gradient-to-r from-gray-900 via-purple-500/85 to-gray-500/50 p-4 flex justify-between items-center">
+    <nav className="backdrop-blur-md bg-gradient-to-r from-gray-900 via-purple-950/85 to-gray-500/50 p-4 flex justify-between items-center">
       <Link to="/" className="font-bold text-xl">KADEL</Link>
       <div className="flex items-center space-x-4">
         <Link to="/projects" className="hover:text-blue-500">Projects</Link>
