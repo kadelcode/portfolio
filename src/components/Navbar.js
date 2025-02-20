fba7a397fbb12ee5dd32b362489db08395e3a5b5
kadelcode <kadelcode@gmail.com>
2025-02-20 14:53:56 +0100

    Update component styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import ThemeToggle from './ThemeToggle';
 
 const Navbar = () => {
   return (
-    <nav className="p-4 flex justify-between items-center text-white z-10 bg-gray-600 backdrop-blur-sm shadow-lg">
+    <nav className="backdrop-blur-md bg-gradient-to-r from-gray-900 via-purple-500/85 to-gray-500/50 p-4 flex justify-between items-center">
       <Link to="/" className="font-bold text-xl">KADEL</Link>
       <div className="flex items-center space-x-4">
         <Link to="/projects" className="hover:text-blue-500">Projects</Link>
