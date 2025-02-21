98419da99bdae0fb4330c4d856eafd70edd3d38a
kadelcode <kadelcode@gmail.com>
2025-02-21 19:58:47 +0100

    Add more content to page

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -5,27 +5,47 @@ const Skills = () => {
         {
             category: 'Programming Languages',
             items: [
-                { name: 'C', level: 90 },
-                { name: 'Python', level: 75 },
-                { name: 'JavaScript', level: 85},
-                { name: 'Java', level: 50}
+                { name: 'C', level: 90, description: '' },
+                { name: 'Python', level: 75, description: '' },
+                { name: 'JavaScript', level: 85, description: ''},
+                { name: 'Java', level: 50, description: ''}
             ],
         },
         {
             category: 'Framework/Libraries',
             items: [
-                { name: 'Django', level: 95},
-                { name: 'Flask', level: 70},
-                { name: 'React', level: 95},
-                { name: 'Node.js', level: 80},
-                { name: 'Express.js', level: 80},
+                { name: 'Django', level: 95, description: 'A python framework'},
+                { name: 'Flask', level: 70, description: ''},
+                { name: 'React', level: 95, description: ''},
+                { name: 'Node.js', level: 80, description: ''},
+                { name: 'Express.js', level: 80, description: ''},
             ]
         },
         {
             category: 'Version Control',
             items: [
-                { name: 'Git', level: 90},
-                { name: 'GitHub', level: 90},
+                { name: 'Git', level: 90, description: ''},
+                { name: 'GitHub', level: 95, description: ''},
+                { name: 'GitLab', level: 80, description: ''},
+            ]
+        },
+        {
+            category: 'Database Management',
+            items: [
+                { name: 'PostgreSQL', level: 95, description: ''},
+                { name: 'MongoDB', level: 80, description: ''},
+                { name: 'MySQL', level: 80, description: ''},
+            ]
+        },
+        {
+            category: 'DevOps & Cloud',
+            items: [
+                { name: 'AWS', level: 60, description: ''},
+                { name: 'GCP', level: 40, description: ''},
+                { name: 'Render', level: 90, description: ''},
+                { name: 'Docker', level: 40, description: ''},
+                { name: 'Kubernetes', level: 40, description: ''},
+                { name: 'GitHub Actions', level: 90, description: ''},
             ]
         }
     ]
@@ -48,6 +68,9 @@ const Skills = () => {
                                             style={{ width: `${skill.level}%`}}
                                         ></div>
                                     </div>
+                                    <p className="mt-2 text-gray-500 dark:text-gray-400 italic">{/* Optional description */}
+                                        {skill.description}
+                                    </p>
                                 </div>
                             ))}
                         </div>
