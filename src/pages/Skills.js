835955e5cd7cf37ac2e83763dd810febeddd47af
kadelcode <kadelcode@gmail.com>
2025-02-21 16:40:21 +0100

    Update styles

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -32,16 +32,16 @@ const Skills = () => {
 
     return (
         <section className='py-12 bg-gray-100 dark:bg-gray-800 dark:text-gray-50 min-h-screen'>
-            <div className='container mx-3 md:mx-auto'>
+            <div className='mx-5 sm:mx-8 md:mx-auto box-border'>
                 <h2 className='text-3xl font-bold mb-6 text-center'>My Skills</h2>
 
                 {skills.map((category) => (
-                    <div key={category.category} className='mb-8 mx-3 md:mx-7'> {/* Added margin bottom for spacing */}
+                    <div key={category.category} className='text-purple-500 dark:text-purple-400 mb-8 md:mx-7'> {/* Added margin bottom for spacing */}
                         <h3 className='text-2xl font-semibold mb-4'>{category.category}</h3> {/* Category title */}
                         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 box-border'>
                             {category.items.map((skill) => (
-                                <div key={skill.name} className='bg-white dark:bg-gray-700 p-6 rounded-lg shadow box-border'>
-                                    <h4 className='text-xl font-semibold mb-2'>{skill.name}</h4> {/* Skill name */}
+                                <div key={skill.name} className='bg-white dark:bg-gray-700 p-6 rounded-lg shadow box-content'>
+                                    <h4 className='text-xl text-black dark:text-white font-semibold mb-2'>{skill.name}</h4> {/* Skill name */}
                                     <div className='bg-gray-200 dark:bg-gray-600 rounded-full h-2'>
                                         <div
                                             className="bg-purple-500 dark:bg-purple-400 rounded-full h-2"
