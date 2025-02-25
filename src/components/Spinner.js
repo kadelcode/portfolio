0e1ba6598c05587b839eb47aa9bad51d03b1cb27
kadelcode <kadelcode@gmail.com>
2025-02-25 22:07:46 +0100

    Update spinner style

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Spinner = () => {
     return (
-        <div className="flex justify-center items-center h-screen">
-            <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-purple-500"></div>
+        <div className="min-h-screen flex justify-center bg-white bg-opacity-75 z-50">
+            <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-purple-500 mt-52"></div>
         </div>
     );
 };
