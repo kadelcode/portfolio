f9d32648e892efad5cf8c4a098599dd33387b272
kadelcode <kadelcode@gmail.com>
2025-02-26 12:57:50 +0100

    Update framer motion props

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -39,9 +39,9 @@ const Contact = () => {
 
   return (
     <motion.div
-    initial={{ opacity: 0, y: -100 }}
-    animate={{ opacity: 1, y: 0 }}
-    exit={{ opacity: 0, y: -100 }}
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    exit={{ opacity: 0 }}
     className='min-h-screen py-12 bg-gray-100 dark:bg-gray-800 transition-colors duration-300'
     >
       <div className='mx-3'>
