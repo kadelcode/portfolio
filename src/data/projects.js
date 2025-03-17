b7a6067de8bb72dc3a8fb4bfe6b991c6e2025c0e
kadelcode <kadelcode@gmail.com>
2025-03-17 16:12:03 +0100

    Update project data

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,5 +1,20 @@
 const projects = [
     {
+        slug: 'ai-chat-app',
+        title: 'AI Chat Application',
+        description: 'A real-time ai-powered chat application.',
+        imageUrl: '/images/novachat_landing_page.png',
+        technologies: ['React', 'Next.js', 'Firebase'],
+        features: [
+            'Authentication (Google/Auth Providers via Firebase)',
+            'Real-time Chat (Firestore)',
+            'AI-generated Replies (OpenAI API)',
+            'Voice-to-Text (Web Speech API)',
+        ],
+        liveUrl: 'https://novachatai.vercel.app',
+        githubUrl: 'https://github.com/kadelcode/ai-chat-app',
+    },
+    {
         slug: 'saas-landing-page',
         title: 'A SaaS Landing Page',
         description: 'A Software as a Service landing page design.',
@@ -18,15 +33,6 @@ const projects = [
         ],
         liveUrl: 'https://litai.vercel.app/',
         githubUrl: 'https://github.com/kadelcode/saas-landing-page',
-    },
-
-    {
-        slug: 'project-2',
-        title: 'Project 2 Title',
-        description: 'A brief description of Project 2.',
-        technologies: ['Vue.js', 'Firebase'],
-        liveUrl: 'https://project2.com',
-        githubUrl: 'https://github.com/kadelcode/project2',
     },
 
     {
