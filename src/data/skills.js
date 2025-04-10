c11abcc15740f5aa4af18c68ef534e9d9c66039c
kadelcode <kadelcode@gmail.com>
2025-04-10 14:49:10 +0100

    Add skills data

---
+++ src/data/skills.js
@@ -0,0 +1,50 @@
+const skills = [
+    {
+        category: 'Programming Languages',
+        items: [
+            { name: 'C', level: 90, description: '' },
+            { name: 'Python', level: 75, description: '' },
+            { name: 'JavaScript', level: 85, description: ''},
+            { name: 'Java', level: 50, description: ''}
+        ],
+    },
+    {
+        category: 'Framework/Libraries',
+        items: [
+            { name: 'Django', level: 95, description: 'A python framework'},
+            { name: 'Flask', level: 70, description: ''},
+            { name: 'React', level: 95, description: ''},
+            { name: 'Node.js', level: 80, description: ''},
+            { name: 'Express.js', level: 80, description: ''},
+        ]
+    },
+    {
+        category: 'Version Control',
+        items: [
+            { name: 'Git', level: 90, description: ''},
+            { name: 'GitHub', level: 95, description: ''},
+            { name: 'GitLab', level: 80, description: ''},
+        ]
+    },
+    {
+        category: 'Database Management',
+        items: [
+            { name: 'PostgreSQL', level: 95, description: ''},
+            { name: 'MongoDB', level: 80, description: ''},
+            { name: 'MySQL', level: 80, description: ''},
+        ]
+    },
+    {
+        category: 'DevOps & Cloud',
+        items: [
+            { name: 'AWS', level: 60, description: ''},
+            { name: 'GCP', level: 40, description: ''},
+            { name: 'Render', level: 90, description: ''},
+            { name: 'Docker', level: 40, description: ''},
+            { name: 'Kubernetes', level: 40, description: ''},
+            { name: 'GitHub Actions', level: 90, description: ''},
+        ]
+    }
+]
+
+export default skills;
