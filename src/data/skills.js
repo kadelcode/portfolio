3023d1b18e03433f2c43a2d747a449df26038fb6
kadelcode <kadelcode@gmail.com>
2025-05-06 16:07:47 +0100

    Update skills data

--- src/data/skills.js
+++ src/data/skills.js
@@ -2,20 +2,22 @@ const skills = [
     {
         category: 'Programming Languages',
         items: [
-            { name: 'C', level: 90, description: '' },
-            { name: 'Python', level: 75, description: '' },
-            { name: 'JavaScript', level: 85, description: ''},
-            { name: 'Java', level: 50, description: ''}
+            { name: 'C', level: 90, description: 'A general-purpose programming language that is extremely popular, simple, and flexible.' },
+            { name: 'Python', level: 75, description: 'A high-level programming language that is widely used for web development, data analysis, and artificial intelligence.' },
+            { name: 'JavaScript', level: 85, description: 'A high-level, dynamic, untyped, and interpreted programming language.' },
+            { name: 'TypeScript', level: 85, description: 'A superset of JavaScript that adds static typing to the language.' },
+            { name: 'Java', level: 50, description: 'A general-purpose programming language that is class-based and object-oriented.' },
         ],
     },
     {
         category: 'Framework/Libraries',
         items: [
-            { name: 'Django', level: 95, description: 'A python framework'},
-            { name: 'Flask', level: 70, description: ''},
-            { name: 'React', level: 95, description: ''},
-            { name: 'Node.js', level: 80, description: ''},
-            { name: 'Express.js', level: 80, description: ''},
+            { name: 'Django', level: 95, description: 'A popular Python web framework for building web applications'},
+            { name: 'Flask', level: 70, description: 'A lightweight python web framework'},
+            { name: 'React', level: 95, description: 'A JavaScript library for building user interfaces'},
+            { name: 'Next.js', level: 80, description: 'A React framework for server-side rendering and static site generation'},
+            { name: 'Node.js', level: 80, description: 'A JavaScript runtime built on Chrome\'s V8 engine'},
+            { name: 'Express.js', level: 80, description: 'A web application framework for Node.js'},
         ]
     },
     {
