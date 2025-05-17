79de01b3bc38669d730e24c242d407750c108224
kadelcode <kadelcode@gmail.com>
2025-05-17 14:32:34 +0100

    Add new styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { keyframes } = require("framer-motion");
+
 module.exports = {
   darkMode: 'class',
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
@@ -13,6 +15,15 @@ module.exports = {
         "light-mode": "url('../public/images/light-mode-background.jpg')",
         "dark-mode": "url('../public/images/dark-mode-background.jpg')",
       },
+      animation: {
+        marquee: 'marquee 20s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-50%)' },
+        }
+      }
     },
   },
   plugins: [],
