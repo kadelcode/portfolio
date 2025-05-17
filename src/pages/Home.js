b2de41210f717a0d60e7318a3d861bd6b61f0c3d
kadelcode <kadelcode@gmail.com>
2025-05-17 16:07:42 +0100

    Update page structure

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -25,7 +25,7 @@ const Home = () => {
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       className=
-      "min-h-screen flex items-center justify-center p-4 relative transition-colors duration-300"
+      "min-h-screen w-full flex flex-col items-center overflow-x-hidden justify-center relative transition-colors duration-300"
     >
       <div
       className="absolute max-h-full inset-0 z-0 mb-0 bg-cover bg-light-mode opacity-20 dark:bg-dark-mode dark:opacity-40"
@@ -33,8 +33,8 @@ const Home = () => {
         backgroundSize: 'cover',
       }}
       ></div>
-      <div className="text-center z-10 text-white md:ml-7 md:mr-7">
-        <h1 className="text-5xl md:text-7xl font-quicksand text-purple-500  dark:text-purple-400 font-bold mb-4"><TypingEffect /></h1><br />
+      <div className="text-center z-10 text-white mx-auto p-8">
+        <h1 className="text-5xl md:text-7xl font-quicksand text-purple-500  dark:text-purple-400 font-bold mb-4 mx-4"><TypingEffect /></h1><br />
         <motion.p
           initial={{ opacity: 0, y: 100 }}
           animate={{ opacity: 1, y: 0 }}
@@ -47,16 +47,8 @@ const Home = () => {
           user-friendly applications that solve real-world problems.
         </motion.p>
 
-        <motion.div
-          initial={{ opacity: 0, y: 100 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: 100 }}
-          className='flex justify-center items-center py-4 max-w-[336px] sm:max-w-lg md:max-w-4xl mx-auto mt-20'
-        >
-          <TechStackMarquee />
-        </motion.div>
-
       </div>
+      <TechStackMarquee />
     </motion.div>
   );
 };
